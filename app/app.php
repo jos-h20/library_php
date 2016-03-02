d1c7d2b8f304d2b6d7cb8aee077b55ef358af9a2
Jordan Meier and Josh Overly <joshoverly@yahoo.com>
2016-03-02 15:25:01 -0800

    add routes in app.php pertaining to librarian user interface

--- app/app.php
+++ app/app.php
@@ -1,6 +1,9 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/ClassName.php";
+    require_once __DIR__."/../src/Author.php";
+    require_once __DIR__."/../src/Book.php";
+    require_once __DIR__."/../src/Copy.php";
+    require_once __DIR__."/../src/Patron.php";
 
     $server = 'mysql:host=localhost;dbname=library';
 	$username = 'root';
@@ -13,8 +16,53 @@
     'twig.path' => __DIR__.'/../views'
     ));
 
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
     $app->get("/", function() use ($app) {
         return $app['twig']->render('index.html.twig');
+    });
+
+    $app->get("/librarians", function() use ($app) {
+        return $app['twig']->render('books.html.twig', array('books' => Book::getAll()));
+    });
+
+    $app->get("/patrons", function() use ($app) {
+        return $app['twig']->render('patrons.html.twig');
+    });
+
+    //////LIBRARIAN/////////
+    $app->post("/add_book", function() use ($app) {
+        $title = $_POST['title'];
+        $id = null;
+        $new_book = new Book($title, $id);
+        $new_book->save();
+        return $app['twig']->render('books.html.twig', array('books' => Book::getAll()));
+    });
+    $app->post("/delete_all_books", function() use ($app) {
+        Book::deleteAll();
+        return $app['twig']->render('books.html.twig', array('books' => Book::getAll()));
+    });
+    $app->get("/book/{id}", function($id) use($app) {
+        $book = Book::find($id);
+        return $app['twig']->render('book.html.twig', array('book' => $book, 'authors' => $book->getCourses(), 'all_authors' => Author::getAll()));
+    });
+    $app->post("/book_add_author", function() use ($app) {
+        $author = Author::find($_POST['author_id']);
+        $book = Book::find($_POST['book_id']);
+        $book->addAuthor($author);
+        return $app['twig']->render('book.html.twig', array('book' => $book, 'authors' => $book->getAuthors(), 'all_authors' => Author::getAll()));
+    });
+    $app->patch("/book/{id}/update", function($id) use ($app) {
+        $book = Book::find($id);
+        $new_title = $_POST['new_title'];
+        $book->update($new_title);
+        return $app['twig']->render('book.html.twig', array('book' => $book, 'authors' => $book->getAuthors(), 'all_authors' => Author::getAll()));
+    });
+    $app->delete("/book/{id}/delete", function($id) use ($app) {
+        $book = Book::find($id);
+        $book->delete();
+        return $app['twig']->render("books.html.twig", array('books' => Book::getAll()));
     });
 
 
